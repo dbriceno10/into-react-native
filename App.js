4e5ff5cee5855a88a729102ef969be16dd3b6d2a
Daniel Briceno <daniel.b100726@gmail.com>
2023-05-11 22:40:14 -0400

    Clase 11: Propiedades por defecto: defaultProps

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Curso React Native</Text>
-      <Saludar name='Daniel'/>
+      <Saludar/>
     </View>
   );
 }
--- src/components/Saludar.js
+++ src/components/Saludar.js
@@ -1,10 +1,28 @@
+// import { View, Text } from 'react-native'
+// import React from 'react'
+
+// export default function Saludar({name = 'Nombre por Defecto'}) {
+//   return (
+//     <View>
+//       <Text>{`Hola ${name}`}</Text>
+//     </View>
+//   )
+// }
+
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function Saludar({name}) {
+export default function Saludar(props) {
+
+  const {firstname, lastname} = props
   return (
     <View>
-      <Text>{`Hola ${name}`}</Text>
+      <Text>{`Hola ${firstname} ${lastname}`}</Text>
     </View>
   )
+}
+
+Saludar.defaultProps = {
+  firstname: 'Daniel',
+  lastname: 'Briceno'
 }
