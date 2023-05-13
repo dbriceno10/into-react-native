6472c65c8a3f28c8c997c87b480762158d9c2fc2
Daniel Briceno <daniel.b100726@gmail.com>
2023-05-12 23:20:59 -0400

    Clase 16: Drawer navigation y próximos pasos

--- App.js
+++ App.js
@@ -4,12 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import NavigationStack from './src/navigations/NavigationStack';
 import NavigationTab from './src/navigations/NavigationTab';
+import NavigationDrawer from './src/navigations/NavigationDrawer';
 
 export default function App() {
   return (
     <NavigationContainer>
       {/* <NavigationStack /> */}
-      <NavigationTab/>
+      {/* <NavigationTab/> */}
+      <NavigationDrawer />
     </NavigationContainer>
 
   );
