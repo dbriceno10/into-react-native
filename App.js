e6742c32a8a11171875b5720e32bb22bfec648c6
Daniel Briceno <daniel.b100726@gmail.com>
2023-05-12 22:44:07 -0400

    Clase 12: Instalando y configurando React Navigation

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 import LoginForm from './src/components/LoginForm';
 import Saludar from './src/components/Saludar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Curso React Native</Text>
-      <Saludar/>
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Curso React Native</Text>
+        <Saludar />
+      </View>
+    </NavigationContainer>
+
   );
 }
 
